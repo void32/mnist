19abd7f7a9e295fbcbd869a52c4d5d90df782703
void32 <mortenalbecknielsen@gmail.com>
2018-06-24 15:26:39 +0200

    Added troubleshooting section in comments

--- examples/visualize_any_graph.py
+++ examples/visualize_any_graph.py
@@ -11,6 +11,10 @@ and visit the webpagefor the local tensorbord at http://localhost:6006
 You can simply create a session just to write the graph to the FileWriter and not do anything else
 See https://stackoverflow.com/questions/48391075/is-it-possible-to-visualize-a-tensorflow-graph-without-a-training-op
 
+Troubleshooting:
+Use global_variables_initializer to initialize all variables, see https://stackoverflow.com/questions/36007883/tensorflow-attempting-to-use-uninitialized-value-in-variable-initialization
+    init = tf.initialize_all_variables()
+    sess.run(init)
 """
 
 import tensorflow as tf
@@ -26,5 +30,6 @@ h = tf.multiply(g, f)
 
 with tf.Session() as sess:
     writer = tf.summary.FileWriter("output", sess.graph)
-    print(sess.run(h))
+    sess_run_out = sess.run(h)
+    print(sess_run_out)
     writer.close()
